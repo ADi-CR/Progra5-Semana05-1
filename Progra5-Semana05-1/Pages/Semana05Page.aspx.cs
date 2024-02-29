750d02a1539ca5e96caa96bef42959c3415093c0
Allan Delgado C <allandelgadoc@gmail.com>
2024-02-28 20:00:42 -0600

    Add project files.

---
+++ Progra5-Semana05-1/Pages/Semana05Page.aspx.cs
@@ -0,0 +1,41 @@
+﻿using Progra5_Semana05_1.DbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Progra5_Semana05_1.Pages
+{
+    public partial class Semana05Page : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+			try
+			{
+				//la implementación de using asegura que el objeto que se crea
+				//sea destruido auntomáticamente una vez que salimos del bloque de código
+				//using
+				using (Progra5_Ejemplo1Entities db = new Progra5_Ejemplo1Entities())
+				{
+					//usamos el procedimiento almacenado para cargar una lista que se
+					//mostrará en el gridview de la páginca 
+
+					var Lista = db.SPUsuariosListar().ToList();
+
+					GvListaUsuarios.DataSource = Lista;
+					GvListaUsuarios.DataBind();
+
+				}
+
+			}
+			catch (Exception)
+			{
+				Response.Redirect("~/Pages/Error.aspx");			
+			}
+
+        }
+    }
+}
---
+++ Progra5-Semana05-1/Pages/UsuarioAgregarPage.aspx.cs
@@ -0,0 +1,46 @@
+﻿using Progra5_Semana05_1.DbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Progra5_Semana05_1.Pages
+{
+    public partial class UsuarioAgregarPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            //primero vamos a capturar en variables locales los valores
+            //digitados en la página. 
+            string nombre = TxtNombre.Text.Trim();
+            string email = TxtEmail.Text.Trim();
+            string telefono = TxtTelefono.Text.Trim();
+            string contrasennia = TxtContrasennia.Text.Trim();
+
+            //en este ejemplo el rol será opcional
+            //TODO: dar funcionalidad al combo para seleccionar el rol 
+
+            try
+            {
+                using (Progra5_Ejemplo1Entities db = new Progra5_Ejemplo1Entities())
+                {
+                    db.SPUsuarioAgregar(nombre, email, telefono, contrasennia, 2);
+                }
+            }
+            catch (Exception)
+            {
+                Response.Redirect("~/Pages/Error.aspx");
+            }
+
+            Response.Redirect("~/Pages/ExitoAgregarUsuario.aspx");
+
+        }
+    }
+}
